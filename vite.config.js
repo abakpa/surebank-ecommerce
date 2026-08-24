5332e3b8ac3c9948e983bc70840e14e6034fd871
joseph abakpa <elachi4c@yahoo.com>
2026-08-25 00:11:12 +0100

    image pagination

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     port: 3002,
     proxy: {
       '/api': {
-        target: 'https://surebank-backend.onrender.com',
+        // target: 'https://surebank-backend.onrender.com',
+        target: 'https://backend-hz3lu.ondigitalocean.app',
         // target: 'http://localhost:8080',
         changeOrigin: true,
       },
